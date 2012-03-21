689dff6d73d993ef72808fa416e072f210ce60b7
BobZhome <BobZhome@Cheerful.com>
2012-03-20 20:38:38 -0400

    One of these prevent decrease poll interval after call. By mik9

--- services/sensorservice/SensorDevice.h
+++ services/sensorservice/SensorDevice.h
@@ -48,6 +48,8 @@ class SensorDevice : public Singleton<SensorDevice> {
     // fixed-size array after construction
     struct Info {
         Info() { }
+        Info() : count(0) { }
+        int32_t count;
         KeyedVector<void*, nsecs_t> rates;
     };
     DefaultKeyedVector<int, Info> mActivationCount;
